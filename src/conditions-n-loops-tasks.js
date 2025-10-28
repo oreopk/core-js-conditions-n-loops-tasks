1cace574ffbe342022a30f5da5e14a57c13bbf8d
oreopk <sart6zaV@yandex.ru>
2025-10-29 00:06:15 +0300

    feat: canQueenCaptureKing done

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -73,8 +73,17 @@ function getMaxNumber(a, b, c) {
  * {x: 1, y: 1}, {x: 2, y: 8} => false
  * {x: 1, y: 1}, {x: 2, y: 8} => false
  */
-function canQueenCaptureKing(/* queen, king */) {
-  throw new Error('Not implemented');
+function canQueenCaptureKing(queen, king) {
+  if (queen.x === king.x || queen.y === king.y) {
+    return true;
+  }
+
+  for (let i = -7; i <= 7; i += 1) {
+    if (queen.x + i === king.x && queen.y + i === king.y) return true;
+    if (queen.x + i === king.x && queen.y - i === king.y) return true;
+  }
+
+  return false;
 }
 
 /**
